987aab74d4290968bdcec4b6142c807c4361efbe
Mosa6 <tikamongi@gmail.com>
2022-07-16 08:49:19 +0000

    delete

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "942bc53b81a246c9ab41718b89dfcaec.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
 end
